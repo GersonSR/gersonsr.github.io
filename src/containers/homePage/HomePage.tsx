4dae5db79caec2af56f06ad2132ab7346e687286
GersonSR <salmeron.gerson@gmail.com>
2025-05-13 23:34:06 -0400

    Added bottom scrolling effect.

--- src/containers/homePage/HomePage.tsx
+++ src/containers/homePage/HomePage.tsx
@@ -15,9 +15,29 @@ function HomePage() {
     const loop = horizontalLoop(titles, { repeat: -1 });
   });
 
+  useGSAP(() => {
+    const titles = gsap.utils.toArray(".box");
+    const colors = ["#0ae448", "#9d95ff", "#abff84", "#00bae2"];
+    gsap.set(titles, {
+      backgroundColor: gsap.utils.wrap(colors),
+    });
+    const loop = horizontalLoop(titles, { repeat: -1, reversed: true });
+  }, {scope: "#footer-wrapper"});
+
+  
+
   return (
     <>
       <div id="wrapper" className={styles.wrapper}>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+      </div>
+      <div id="footer-wrapper" className={styles.footerWrapper}>
         <div className={`${styles.title} box`}>Under Construction</div>
         <div className={`${styles.title} box`}>Under Construction</div>
         <div className={`${styles.title} box`}>Under Construction</div>
