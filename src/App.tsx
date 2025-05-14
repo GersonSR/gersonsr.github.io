544047dc52740580405837b54dba09172535eeee
GersonSR <salmeron.gerson@gmail.com>
2025-05-13 21:53:47 -0400

    Added infinite scrolling header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Root, { routes } from './components/root/Root';
-import HomePage from './containers/homePage/homePage';
+import HomePage from './containers/homePage/HomePage';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path={routes.HOME} element={<Root />}>
--- src/containers/homePage/HomePage.tsx
+++ src/containers/homePage/HomePage.tsx
@@ -1,10 +1,33 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { horizontalLoop } from "../../components/gsapHelperFunctions/GSAPHelperFunctions";
+import styles from "../../modules/homePage/HomePage.module.css";
+
+gsap.registerPlugin(useGSAP);
 
 function HomePage() {
-    return(
-        <>
-            <p>Home Page</p>
-        </>
-    );
-};
+  useGSAP(() => {
+    const titles = gsap.utils.toArray(".box");
+    const colors = ["#0ae448", "#9d95ff", "#abff84", "#00bae2"];
+    gsap.set(titles, {
+      backgroundColor: gsap.utils.wrap(colors),
+    });
+    const loop = horizontalLoop(titles, { repeat: -1 });
+  });
+
+  return (
+    <>
+      <div id="wrapper" className={styles.wrapper}>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+        <div className={`${styles.title} box`}>Under Construction</div>
+      </div>
+    </>
+  );
+}
 
 export default HomePage;
