a9458dca7f08d44faef9cf3e20bb2f3b8db05e1f
GersonSR <salmeron.gerson@gmail.com>
2025-05-17 15:28:42 -0400

    Added random string generator for the profile pics.

--- src/components/infoCard/InfoCard.tsx
+++ src/components/infoCard/InfoCard.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import styles from '../../modules/infoCard/InfoCard.module.css'
+
+// Random String Generator sourced from: https://sentry.io/answers/generate-random-string-characters-in-javascript/
+function createRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+
 function InfoCard() {
     return (
         <>
