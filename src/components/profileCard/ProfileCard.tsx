34f0315f7ad5bd3086b33b9a2d2979419219236d
GersonSR <salmeron.gerson@gmail.com>
2025-05-17 23:13:24 -0400

    cool scrolling border for the contact me sub footer.

--- src/components/profileCard/ProfileCard.tsx
+++ src/components/profileCard/ProfileCard.tsx
@@ -2,6 +2,9 @@ import Avatar from "boring-avatars";
 import styles from "../../modules/profileCard/ProfileCard.module.css";
 import githubIcon from "../../assets/icons/bx-github.svg";
 import linkedInIcon from "../../assets/icons/bx-linkedin-square.svg";
+import gsap from "gsap";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
+import { useGSAP } from "@gsap/react";
 
 // Random String Generator sourced from: https://sentry.io/answers/generate-random-string-characters-in-javascript/
 function createRandomString(length: number) {
@@ -14,9 +17,28 @@ function createRandomString(length: number) {
   return result;
 }
 
+gsap.registerPlugin(DrawSVGPlugin, useGSAP);
+
 function ProfileCard() {
   const randomLength = Math.random() * 10;
   const randomString = createRandomString(randomLength);
+
+  useGSAP(() => {
+    gsap.set("#drive", { drawSVG: false });
+
+    gsap
+      .timeline({ delay: 0.75 })
+      .to("#drive", {
+        drawSVG: "0% 10%",
+        ease: "power1.in",
+      })
+      .to("#drive", {
+        drawSVG: "100% 110%",
+        ease: "power1.out",
+        repeat: -1,
+        duration: 3
+      });
+  });
 
   return (
     <div className={`${styles.profileContainer}`}>
@@ -80,6 +102,17 @@ function ProfileCard() {
         </div>
       </div>
       <div className={`${styles.contactMe}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="5px">
+          <line className={`${styles.path}`} x1="0" y1="50%" x2="100%" y2="50%" />
+          <line
+            id="drive"
+            className={`${styles.target}`}
+            x1="0"
+            y1="50%"
+            x2="100%"
+            y2="50%"
+          />
+        </svg>
         <a href="mailto:salmeron.gerson@gmail.com" className={`${styles.link}`}>
           <span>Contact Me</span>
         </a>
